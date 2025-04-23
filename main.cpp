820051a2b8cea40fb978ba9cf37b1865525de942
kitajima-hayato <k023g0057@g.neec.ac.jp>
2025-04-23 14:47:55 +0900

    a

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//_CrtSetBreakAlloc();
 	MyGame game;
 	game.Run();
-
+	 
 	_CrtDumpMemoryLeaks();
 	// メモリリーク検出
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
